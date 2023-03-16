53f92951bb8cd67180b93e5f28dc1d984833fbf0
ra230613 <l230613@dac.unicamp.br>
2023-03-16 18:30:42 -0300

    cliente semi completo

--- Cliente.java
+++ Cliente.java
@@ -48,6 +48,55 @@ public class Cliente {
 	    this.endereco = endereco;
 	} 
 	
+	public String toString() {
+		return nome;
+	}
+	
+	public boolean validarCPF(String cpf) {
+		
+		// Removendo caracteres nao numericos
+		String cpfNumerico = cpf.replaceAll( "[^\\d]", "" );
+		if (cpfNumerico.length() != 11) return false;
+	
+		// Verificando se todos os dígitos não são iguais
+		char firstDigit = cpfNumerico.charAt(0);
+		int validador = 0;
+		for (int i = 0; i < 11; i++) {
+			if(cpfNumerico.charAt(i) != firstDigit) {
+				validador = 1;
+				break;
+			}
+		}
+		if (validador == 0) return false;
+		
+		//Calculando e validando os dígitos verificadores
+		// calculando o primeiro digito
+		// falta verificar se o resto for maior que 10 o numero vira 0
+		
+		char primeiroDigitoVerificador = cpfNumerico.charAt(9);
+		char segundoDigitoVerificador = cpfNumerico.charAt(10);
+		int somaPrimeiro = 0, somaSegundo = 0;
+		
+		for(int i = 0; i < 9; i++) {
+			somaPrimeiro += cpfNumerico.charAt(i)*(10 - i);
+		}
+		
+		int restoPrimeiro = somaPrimeiro % 11;
+		int resultadoPrimeiro = 11 - restoPrimeiro;
+		if(resultadoPrimeiro != primeiroDigitoVerificador) return false;
+		
+		//calculando o segundo digito
+		
+		for(int j = 0; j < 10; j++) {
+			somaSegundo += cpfNumerico.charAt(j)*(11 - j);
+		}
+		int restoSegundo = somaSegundo % 11;
+		int resultadoSegundo = 11 - restoSegundo;
+		if(resultadoSegundo != segundoDigitoVerificador) return false;
+		
+		
+		
+	}
 	
 
 	
